35f2f4ded7e5b0e8afb88297bf8c90be6ac57c14
Vijeth Hegde <vijethshegde7@gmail.com>
2025-10-02 21:43:00 +0530

    second commit

--- src/app/components/login.ts
+++ src/app/components/login.ts
@@ -1,9 +1,14 @@
-
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule, FormBuilder, Validators, FormGroup } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormBuilder,
+  Validators,
+  FormGroup,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { MATERIAL_IMPORTS } from '../shared/material.imports';
 
 @Component({
@@ -12,59 +17,63 @@ import { MATERIAL_IMPORTS } from '../shared/material.imports';
   imports: [CommonModule, ReactiveFormsModule, ...MATERIAL_IMPORTS],
   templateUrl: './login.html',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
-  error: string | null = null;
   loading = false;
 
   constructor(
     private fb: FormBuilder,
     private http: HttpClient,
-    private router: Router
-  ) {
+    private router: Router,
+    private snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit() {
     this.loginForm = this.fb.group({
       username: ['', Validators.required],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
     });
   }
 
   onSubmit() {
     if (this.loginForm.invalid) return;
-
     const { username, password } = this.loginForm.value;
-    this.error = null;
     this.loading = true;
 
-    this.http.post<{ token: string }>(
-      'api/users/login',
-      { username, password },
-      { headers: { 'Content-Type': 'application/json' } }
-    ).subscribe({
-      next: (res: any) => {
-        localStorage.setItem('authToken', res?.accessToken);
-        if(res?.accessToken){
-          this.router.navigate(['/practice-tests']);
-        }
-        // snackBar.open('Message archived');
-        this.loading = false;
-      },
-      error: (err) => {
-        this.error = err;
-        this.loading = false;
-        console.error('Login error:', err);
-      }
-    });
+    this.http
+      .post<{ accessToken: string }>(
+        'api/users/login',
+        { username, password },
+        { headers: { 'Content-Type': 'application/json' } }
+      )
+      .subscribe({
+        next: (res) => {
+          this.loading = false;
+          if (res?.accessToken) {
+            localStorage.setItem('authToken', res.accessToken);
+            this.snackBar.open('Login successful!', '', {
+              duration: 3000,
+              panelClass: ['mat-toolbar', 'mat-primary'],
+            });
+
+            this.router.navigate(['/practice-tests']);
+          } else {
+            this.snackBar.open('Login failed: No token returned', '', {
+              duration: 3000,
+              panelClass: ['mat-toolbar', 'mat-success'],
+            });
+          }
+        },
+        error: (err) => {
+          this.loading = false;
+          this.snackBar.open(
+            `Login error: ${
+              err?.error?.message || err.message || 'Unknown error'
+            }`,
+            '',
+            { duration: 3000,panelClass: ['mat-toolbar', 'mat-warn'] }
+          );
+        },
+      });
   }
 }
-
-
-// this.http.post<{ token: string }>(
-//   'https://reqres.in/api/login',
-//   { email: usernameOrEmail, password }
-// ).subscribe({
-//   next: res => {
-//     localStorage.setItem('authToken', res.token);
-//     this.router.navigate(['/todo']); // or wherever
-//   },
-//   error: err => { this.error = 'Login failed'; }
-// });
